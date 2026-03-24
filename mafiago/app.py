712aa60e2fd7420dd1aacebe6f3eacac3183820e
universestudio-koreanempire <jiwhan.park11@gmail.com>
2026-03-25 07:50:58 +0900

    Add jsonify import to app.py

--- mafiago/app.py
+++ mafiago/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from datetime import datetime
 import sqlite3
 import hashlib
